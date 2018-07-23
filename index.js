af8a1240d4535e22c70e42c8011ce4d65c0098e3
Peter Vermeulen <contact@valkyriestudios.be>
2018-07-23 03:16:33 +0200

    Adjust legacy require statement to be setupMaster from node cluster'

--- index.js
+++ index.js
@@ -38,92 +38,90 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 //  Parse incoming parameters using minimist
 var params = (0, _nodeArgs2.default)();
-console.log(params);
+var config_worker = params.flags.w !== undefined ? params.flags.w : params.flags.worker;
+var config_count = params.flags.c !== undefined ? params.flags.c : params.flags.count;
+var config_timeout = params.flags.t !== undefined ? params.flags.t : params.flags.timeout;
 
 var scope = (0, _freeze2.default)({
-    path: _path2.default.resolve(params.flags.w || params.flags.worker),
-    amt: params.flags.c || params.flags.count || _os2.default.cpus().length,
-    timeout: params.flags.t || params.flags.timeout || 10000,
+    path: _path2.default.relative(process.cwd(), _path2.default.resolve(config_worker)),
+    amt: config_count || _os2.default.cpus().length,
+    timeout: config_timeout || 10000,
     name: 'web',
     instances: {}
 });
 
-if (_cluster2.default.isMaster) {
-    //  MASTER BOOTSTRAP
-    var utils = (0, _defineProperties2.default)((0, _create2.default)(null), {
-        logWorker: {
-            value: function value(inst, msg) {
-                return console.log('worker::' + inst.process.pid + ' || ' + msg);
-            }
-        },
-        logMaster: {
-            value: function value(inst, msg) {
-                return console.log('master [worker::' + inst.process.pid + '] || ' + msg);
-            }
-        },
-        clean: {
-            value: function value(inst) {
-                var msg = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : !1;
-
-                if (msg) {
-                    utils.logMaster(inst, msg);
-                }
-                clearTimeout((scope.instances[inst.process.pid] || {}).ttl || null);
-                delete scope.instances[inst.process.pid];
-            }
-        },
-        noop: {
-            value: function value() {}
+//  MASTER BOOTSTRAP
+var utils = (0, _defineProperties2.default)((0, _create2.default)(null), {
+    logWorker: {
+        value: function value(inst, msg) {
+            return console.log('worker::' + inst.process.pid + ' || ' + msg);
         }
-    });
-
-    //  Listen for fork events and register a timer that checks for process completeness
-    _cluster2.default.on('fork', function (inst) {
-        utils.logWorker(inst, 'forked');
-
-        scope.instances[inst.process.pid] = {
-            proc: inst,
-            ttl: setTimeout(function () {
-                utils.clean(inst);
-                utils.logWorker(inst, 'timeout');
-            }, params.flags.timeout)
-        };
-    });
-
-    //  Log a message for each worker starting
-    _cluster2.default.on('online', function (inst) {
-        utils.logWorker(inst, 'online');
-    });
-
-    //  Log a message for each worker starting
-    _cluster2.default.on('listening', function (inst, addr) {
-        utils.clean(inst);
-        utils.logWorker(inst, 'listening ' + (addr.address || '127.0.0.1') + ':' + addr.port);
-    });
-
-    //  Log a message if the worker is shutting down
-    _cluster2.default.on('exit', function (inst, code, signal) {
-        utils.clean(inst, 'shutdown');
-    });
-
-    //  A clustered worker can communicate with the master through means of an ipc channel
-    //  these messages have types that need to be complacent with the ones defined in the master's register
-    _cluster2.default.on('message', function (inst, msg, handle) {
-        var _WORKER_DAEMON_EVENTS;
-
-        ((_WORKER_DAEMON_EVENTS = {}, (0, _defineProperty3.default)(_WORKER_DAEMON_EVENTS, _WorkerDaemon.WORKER_DAEMON_EVENTS.SHUTDOWN, function () {
-            utils.clean(inst, 'shutting down | reason : ' + msg.data);
-            inst.kill();
-        }), (0, _defineProperty3.default)(_WORKER_DAEMON_EVENTS, _WorkerDaemon.WORKER_DAEMON_EVENTS.LOG, function () {
-            utils.logMaster(inst, msg.data);
-        }), _WORKER_DAEMON_EVENTS)[msg.type] || utils.noop)();
-    });
-
-    //  Fork the exact amount of cpu cores as workers
-    for (var i = 0; i < scope.amt; i++) {
-        _cluster2.default.fork();
+    },
+    logMaster: {
+        value: function value(inst, msg) {
+            return console.log('master [worker::' + inst.process.pid + '] || ' + msg);
+        }
+    },
+    clean: {
+        value: function value(inst) {
+            clearTimeout((scope.instances[inst.process.pid] || {}).ttl || null);
+            delete scope.instances[inst.process.pid];
+        }
+    },
+    noop: {
+        value: function value() {}
     }
-} else {
-    //  WORKER BOOTSTRAP
-    require(scope.path);
+});
+
+//  Listen for fork events and register a timer that checks for process completeness
+_cluster2.default.on('fork', function (inst) {
+    utils.logWorker(inst, 'forked');
+
+    scope.instances[inst.process.pid] = {
+        proc: inst,
+        ttl: setTimeout(function () {
+            utils.clean(inst);
+            utils.logWorker(inst, 'timeout');
+        }, scope.timeout)
+    };
+});
+
+//  Log a message for each worker starting
+_cluster2.default.on('online', function (inst) {
+    utils.logWorker(inst, 'online');
+});
+
+//  Log a message for each worker starting
+_cluster2.default.on('listening', function (inst, addr) {
+    utils.clean(inst);
+    utils.logWorker(inst, 'listening ' + (addr.address || '127.0.0.1') + ':' + addr.port);
+});
+
+//  Log a message if the worker is shutting down
+_cluster2.default.on('exit', function (inst, code, signal) {
+    utils.clean(inst);
+});
+
+//  A clustered worker can communicate with the master through means of an ipc channel
+//  these messages have types that need to be complacent with the ones defined in the master's register
+_cluster2.default.on('message', function (inst, msg, handle) {
+    var _WORKER_DAEMON_EVENTS;
+
+    ((_WORKER_DAEMON_EVENTS = {}, (0, _defineProperty3.default)(_WORKER_DAEMON_EVENTS, _WorkerDaemon.WORKER_DAEMON_EVENTS.SHUTDOWN, function () {
+        utils.logMaster(inst, 'shutting down | reason : ' + msg.data);
+        utils.clean(inst);
+        inst.kill();
+    }), (0, _defineProperty3.default)(_WORKER_DAEMON_EVENTS, _WorkerDaemon.WORKER_DAEMON_EVENTS.LOG, function () {
+        utils.logMaster(inst, msg.data);
+    }), _WORKER_DAEMON_EVENTS)[msg.type] || utils.noop)();
+});
+
+//  Configure master
+_cluster2.default.setupMaster({
+    exec: scope.path
+});
+
+//  Fork the exact amount of cpu cores as workers
+for (var i = 0; i < scope.amt; i++) {
+    _cluster2.default.fork();
 }
--- src/index.js
+++ src/index.js
@@ -7,86 +7,86 @@ import NodeArgs                 from '@valkyriestudios/node-args';
 import { WORKER_DAEMON_EVENTS } from './WorkerDaemon';
 
 //  Parse incoming parameters using minimist
-const params = NodeArgs();
-console.log(params);
+const params            = NodeArgs();
+const config_worker     = params.flags.w !== undefined ? params.flags.w : params.flags.worker;
+const config_count      = params.flags.c !== undefined ? params.flags.c : params.flags.count;
+const config_timeout    = params.flags.t !== undefined ? params.flags.t : params.flags.timeout;
 
 const scope = Object.freeze({
-    path        : path.resolve(params.flags.w || params.flags.worker),
-    amt         : params.flags.c || params.flags.count || os.cpus().length,
-    timeout     : params.flags.t || params.flags.timeout || 10000,
+    path        : path.relative(process.cwd(), path.resolve(config_worker)),
+    amt         : config_count || os.cpus().length,
+    timeout     : config_timeout || 10000,
     name        : 'web',
     instances   : {},
 });
 
-if (cluster.isMaster) {
-    //  MASTER BOOTSTRAP
-    const utils = Object.defineProperties(Object.create(null), {
-        logWorker : {
-            value : (inst, msg) => console.log(`worker::${inst.process.pid} || ${msg}`),
+//  MASTER BOOTSTRAP
+const utils = Object.defineProperties(Object.create(null), {
+    logWorker : {
+        value : (inst, msg) => console.log(`worker::${inst.process.pid} || ${msg}`),
+    },
+    logMaster : {
+        value : (inst, msg) => console.log(`master [worker::${inst.process.pid}] || ${msg}`)
+    },
+    clean : {
+        value : (inst) => {
+            clearTimeout((scope.instances[inst.process.pid] || {}).ttl || null);
+            delete scope.instances[inst.process.pid];
+        }
+    },
+    noop : {
+        value : () => {}
+    },
+});
+
+//  Listen for fork events and register a timer that checks for process completeness
+cluster.on('fork', (inst) => {
+    utils.logWorker(inst, 'forked');
+
+    scope.instances[inst.process.pid] = {
+        proc : inst,
+        ttl : setTimeout(() => {
+            utils.clean(inst);
+            utils.logWorker(inst, 'timeout');
+        }, scope.timeout),
+    };
+});
+
+//  Log a message for each worker starting
+cluster.on('online', (inst) => {
+    utils.logWorker(inst, 'online');
+});
+
+//  Log a message for each worker starting
+cluster.on('listening', (inst, addr) => {
+    utils.clean(inst);
+    utils.logWorker(inst, `listening ${addr.address || '127.0.0.1'}:${addr.port}`);
+});
+
+//  Log a message if the worker is shutting down
+cluster.on('exit', (inst, code, signal) => {
+    utils.clean(inst);
+});
+
+//  A clustered worker can communicate with the master through means of an ipc channel
+//  these messages have types that need to be complacent with the ones defined in the master's register
+cluster.on('message', (inst, msg, handle) => {
+    ({
+        [WORKER_DAEMON_EVENTS.SHUTDOWN] : () => {
+            utils.logMaster(inst, `shutting down | reason : ${msg.data}`);
+            utils.clean(inst);
+            inst.kill();
         },
-        logMaster : {
-            value : (inst, msg) => console.log(`master [worker::${inst.process.pid}] || ${msg}`)
+        [WORKER_DAEMON_EVENTS.LOG] : () => {
+            utils.logMaster(inst, msg.data);
         },
-        clean : {
-            value : (inst, msg = false) => {
-                if (msg) {
-                    utils.logMaster(inst, msg);
-                }
-                clearTimeout((scope.instances[inst.process.pid] || {}).ttl || null);
-                delete scope.instances[inst.process.pid];
-            }
-        },
-        noop : {
-            value : () => {}
-        },
-    });
+    }[msg.type] || utils.noop)();
+});
 
-    //  Listen for fork events and register a timer that checks for process completeness
-    cluster.on('fork', (inst) => {
-        utils.logWorker(inst, 'forked');
+//  Configure master
+cluster.setupMaster({
+    exec: scope.path,
+});
 
-        scope.instances[inst.process.pid] = {
-            proc : inst,
-            ttl : setTimeout(() => {
-                utils.clean(inst);
-                utils.logWorker(inst, 'timeout');
-            }, params.flags.timeout),
-        };
-    });
-
-    //  Log a message for each worker starting
-    cluster.on('online', (inst) => {
-        utils.logWorker(inst, 'online');
-    });
-
-    //  Log a message for each worker starting
-    cluster.on('listening', (inst, addr) => {
-        utils.clean(inst);
-        utils.logWorker(inst, `listening ${addr.address || '127.0.0.1'}:${addr.port}`);
-    });
-
-    //  Log a message if the worker is shutting down
-    cluster.on('exit', (inst, code, signal) => {
-        utils.clean(inst, 'shutdown');
-    });
-
-    //  A clustered worker can communicate with the master through means of an ipc channel
-    //  these messages have types that need to be complacent with the ones defined in the master's register
-    cluster.on('message', (inst, msg, handle) => {
-        ({
-            [WORKER_DAEMON_EVENTS.SHUTDOWN] : () => {
-                utils.clean(inst, `shutting down | reason : ${msg.data}`);
-                inst.kill();
-            },
-            [WORKER_DAEMON_EVENTS.LOG] : () => {
-                utils.logMaster(inst, msg.data);
-            },
-        }[msg.type] || utils.noop)();
-    });
-
-    //  Fork the exact amount of cpu cores as workers
-    for (let i = 0; i < scope.amt; i++) cluster.fork();
-} else {
-    //  WORKER BOOTSTRAP
-    require(scope.path);
-}
+//  Fork the exact amount of cpu cores as workers
+for (let i = 0; i < scope.amt; i++) cluster.fork();
